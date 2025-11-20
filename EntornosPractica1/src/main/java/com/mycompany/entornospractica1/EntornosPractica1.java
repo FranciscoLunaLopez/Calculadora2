da124f7aed6cdbc422c01e50f618dc5e27832018
franc <franc@Lacria>
2025-11-20 20:07:07 +0100

    Primer commit

---
+++ EntornosPractica1/src/main/java/com/mycompany/entornospractica1/EntornosPractica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.entornospractica1;
+
+/**
+ *
+ * @author franc
+ */
+public class EntornosPractica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
